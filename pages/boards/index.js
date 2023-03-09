a211a076417a771ea07b304850534a83982b5760
Jhoon <jaehoon0822@gmail.com>
2023-03-09 10:57:07 +0900

    2023-03-09 index.js 내용수정

--- pages/boards/index.js
+++ pages/boards/index.js
@@ -6,4 +6,8 @@ const BoardPage = () => {
   )
 }
 
+// export const getServerSideProps = () => {...}
+// ssr 은 페이지  생성 안됨
+// 만약 원한다면,  next.config.js 에서 exportPathMap 을 이용하여 설정해주어야 한다.
+
 export default BoardPage
