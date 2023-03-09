882baf4bf6039760c9a87f33bb7541c665a032ad
Jhoon <jaehoon0822@gmail.com>
2023-03-09 11:23:13 +0900

    2023-03-09 re commit

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  generateBuildId: () =>"codecamp-deploy-project",
+  generateBuildId: () => "codecamp-deploy-project",
   exportPathMap: () => {
     return {
       "/": {page: "/"},
